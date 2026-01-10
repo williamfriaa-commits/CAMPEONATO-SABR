82f744facbad825bafb8beafab2900a4f0587a98
williamfriaa-commits <254133064+williamfriaa-commits@users.noreply.github.com>
2026-01-10 18:43:44 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,44 @@
-const CACHE_NAME = "sabr-app-v1";
+const CACHE_NAME = "eventos-sabr-v1";
+
 const FILES_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/tabela.html",
-  "/manifest.json"
+  "./",
+  "./index.html",
+  "./tabela.html",
+  "./manifest.json",
+  "./logo.png",
+  "./logo-192.png",
+  "./logo-512.png",
+  "./fundo-pc-1920x1080.png",
+  "./fundo-mobile-1080x1920.png"
 ];
 
+// INSTALA
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(FILES_TO_CACHE);
     })
   );
+  self.skipWaiting();
 });
 
+// ATIVA
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// FETCH
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
